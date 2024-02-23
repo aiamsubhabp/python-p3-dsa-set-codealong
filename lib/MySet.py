c32b93f558a3b94975c744e7619a998b28c443ab
Paul Aiamsubhab <paul.aiamsubhab@gmail.com>
2024-02-23 00:33:46 -0500

    complete lab

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,37 @@
 class MySet:
+    def __init__(self, enumberable = []):
+        self.dictionary = {}
+        for value in enumberable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        # Add a value as a key on the Dictionary
+        self.dictionary[value] = True 
+        # Return the updated set
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+    
+    def __str__(self):
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
+    
+
+# set = MySet([1,2,3,4,5])
+
+# # set.add(7)
+
+# # print(set.__str__())
